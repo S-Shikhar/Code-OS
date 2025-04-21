bcb97155a6cc813cdb3165691661d1b97acb73d3
SOUHARDA SHIKHAR BISWAS <souhardashikharbiswas@gmail.com>
2025-04-22 05:08:52 +0530

    Refactor scheduling algorithms: remove non-functional priority scheduling script, enhance SJF and SRTF implementations, and improve output formatting for process details.

--- practice2/sjf.py
+++ practice2/sjf.py
@@ -18,10 +18,12 @@ bt = [int(input(f"Enter the burst time of process P{i}: "))for i in range(n)]
 wt, ct, tat = [0] * n, [0] * n, [0] * n
 
 #remaining processes
-rp = []
-for i in range(n):
-    rp.append((at[i], bt[i], i))
+# rp = []
+# for i in range(n):
+#     rp.append((at[i], bt[i], i))
     
+rp = [(at[i], bt[i], i) for i in range(n)]
+
 current_time = min(at)
 
 while rp:
@@ -35,7 +37,7 @@ while rp:
         continue
     
     process = min(available_processes, key=lambda x: x[1])
-    pid = process[0]
+    pid = process[2]
     ct[pid] = current_time + process[1]
     tat[pid] = ct[pid] - at[pid]
     wt[pid] = tat[pid] - bt[pid]
@@ -46,3 +48,11 @@ total_wt = sum(wt)
 total_tat = sum(tat)
 average_wt = total_wt / n
 average_tat = total_tat / n
+
+print("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time")
+for i in range(n):
+    print(f"P{i}\t{at[i]}\t\t{bt[i]}\t\t{ct[i]}\t\t{tat[i]}\t\t{wt[i]}")
+    
+print(f"\nAverage Waiting Time: {average_wt:.2f}")
+print(f"Average Turnaround Time: {average_tat:.2f}")
+
