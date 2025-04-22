6f9bb9d5679581c02aa54ab934ea6330b75cf969
SOUHARDA SHIKHAR BISWAS <souhardashikharbiswas@gmail.com>
2025-04-22 06:27:53 +0530

    Refactor factorial script for improved readability and consistency in loop formatting; add new script to calculate days in a month based on user input.

--- practice/fact.sh
+++ practice/fact.sh
@@ -2,7 +2,8 @@ read -p "Enter the number: " n
 
 fact=1
 
-for ((i=1; i<=n; i++)); do
+for ((i=1; i<=n; i++))
+do
 	fact=$((fact*i))
 done
 
--- practice2/add_sub.sh
+++ practice2/add_sub.sh
@@ -1,26 +1,30 @@
 echo "Calculator"
-echo "choose: 1. (+), 2. (-), 3. (*), 4. (/)"
 
+cal() {
+    case $c in
+        "+")
+            read -p "Enter two numbers: " a b
+            echo "$a + $b = $(($a + $b))"
+            ;;
+        "-")
+            read -p "Enter two numbers: " a b
+            echo "$a - $b = $(($a - $b))"
+            ;;
+        "*")
+            read -p "Enter two numbers: " a b
+            echo "$a * $b = $(($a * $b))"
+            ;;
+        "/")
+            read -p "Enter two numbers: " a b
+            echo "$a / $b = $(($a / $b))"
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
+}
+
+echo "choose: 1. (+), 2. (-), 3. (*), 4. (/)"
 read -p "Enter your Choice(1, 2, 3, 4): " c
 
-case $c in
-    1)
-        read -p "Enter two numbers: " a b
-        echo "$a + $b = $(($a + $b))"
-        ;;
-    2)
-        read -p "Enter two numbers: " a b
-        echo "$a - $b = $(($a - $b))"
-        ;;
-    3)
-        read -p "Enter two numbers: " a b
-        echo "$a * $b = $(($a * $b))"
-        ;;
-    4)
-        read -p "Enter two numbers: " a b
-        echo "$a / $b = $(($a / $b))"
-        ;;
-    *)
-        echo "Invalid choice"
-        ;;
-esac
+cal 
